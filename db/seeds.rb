0819f970f7d0ec4c905f9b0077735044b837bab9
kotamori9 <kotamori27@gmail.com>
2020-05-29 02:13:03 +0900

    DB,コントローラー作成完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(category: '初心者歓迎')
+Category.create(category: '経験者募集')
+Category.create(category: 'FW募集')
+Category.create(category: 'BK募集')
+Category.create(category: 'マネージャー募集')
+Category.create(category: '実績あり')
