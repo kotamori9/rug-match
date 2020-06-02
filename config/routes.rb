acf4125eeae4030d0c3283ee5c0e75cf574737d2
kotamori9 <kotamori27@gmail.com>
2020-06-02 22:10:49 +0900

    [WIP]募集一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "tops#index"
-  resources :tops, only: :index
+  resources :tops, only: [:index,:new]
   resources :users, only: [:edit,:update,:show,:destroy]
 end
