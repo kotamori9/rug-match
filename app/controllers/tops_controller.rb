b282daf1da7d0c64e8cd0cd6e1b6a5a6818a3607
kotamori9 <kotamori27@gmail.com>
2020-06-23 13:59:23 +0900

    募集一覧ページ編集

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,13 +1,6 @@
 class TopsController < ApplicationController
-  before_action :set_recruitment, only: [:index]
 
   def index
     @recruitments = Recruitment.all
-    @area = Areahash.find(@recruitment.area)
-  end
-
-  private
-  def set_recruitment
-    @recruitment = Recruitment.find_by(params[:area])
   end
 end
